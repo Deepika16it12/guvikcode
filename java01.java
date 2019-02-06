83c6704ff2f211ffa0cc60b751a82b5856733a1e
Deepika16it12 <47378365+Deepika16it12@users.noreply.github.com>
2019-02-06 11:55:12 +0530

    Create java01.java

---
+++ java01.java
@@ -0,0 +1,19 @@
+class PositiveNegative {
+
+    public static void main(String[] args) {
+
+        int number = 3;
+ if (number < 0)
+ {
+            System.out.println("negative ");
+ }
+ else if ( number > 0)
+ {
+            System.out.println("positive");
+ }
+else
+           {
+               System.out.println( " zero");
+    }
+}
+}
