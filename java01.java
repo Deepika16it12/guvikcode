f6c15be3492df3fea401815d069c2683019796d6
Deepika16it12 <47378365+Deepika16it12@users.noreply.github.com>
2019-02-06 12:08:52 +0530

    Update java01.java

--- java01.java
+++ java01.java
@@ -2,18 +2,18 @@ class PositiveNegative {
 
     public static void main(String[] args) {
 
-        int number = 3;
- if (number > 0)
+        int num = 3;
+ if (num > 0)
  {
             System.out.println("Positive ");
  }
- else if ( number <0)
+ else if ( num < 0)
  {
             System.out.println("Negative ");
  }
 else
            {
-               System.out.println( " zero");
+               System.out.println( "zero");
     }
 }
 }
